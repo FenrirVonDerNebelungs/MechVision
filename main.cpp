deee46bb82835a437492234ca5d83704d0aa7b40
BessPtck <info@mymindsi.com>
2022-01-31 17:45:53 -0800

    return from test on pi

--- main.cpp
+++ main.cpp
@@ -19,17 +19,19 @@ using namespace cv;
 
 int runTest0();
 int testPi();
+int testLive();
 int main() {
 	int dummyMarker0;
 	int* pdummyMark0 = &dummyMarker0;
 
-	return runTest0();
+	return testPi();
 }
-
-int testPi() {
-	int dummyMarker0;
-	int* pdummyMark0 = &dummyMarker0;
-	VideoCapture cap("IOFiles/messylines.avi");
+int testLive() {
+	VideoCapture cap;
+	Mat frame;
+	int deviceID = 0;
+	int apiID = cv::CAP_ANY;
+	cap.open(deviceID, apiID);
 	if (!cap.isOpened()) {
 		cout << "Error\n";
 		return -1;
@@ -50,6 +52,72 @@ int testPi() {
 	DrivePlane* driveP = new DrivePlane;
 	driveP->init(findLines);
 
+	DrawHexImg hexDrawLines;
+	hexDrawLines.Init((HexBase*)&HexLow);
+
+	for (int icnt = 0; icnt < 1000; icnt++) {
+		cap.read(frame);
+		if (frame.empty()) {
+			cerr << "ERROR blank frame grabbed\n";
+			break;
+		}
+		unsigned char* frmdat = frame.data;
+		MVImg.initNoOwn(frmdat, frame_width, frame_height, 3L);
+		HexLow.Update(&MVImg);
+
+		patLunLay.Update();
+		findLines->spawn();
+		driveP->update();
+		hexDrawLines.renderLineImg(findLines);
+
+		unsigned char* MVImgdat = hexDrawLines.getHexedImg()->getImg();//doCol ? hexBaseDraw.getHexedImg()->getImg() : hexDraw.getHexedImg()->getImg();//MVImg.getImg();
+		Size frameSize(frame_width, frame_height);
+		Mat rendFrame(frameSize, CV_8UC3, (void*)MVImgdat);
+
+		imshow("TestVid", rendFrame);
+		char c = (char)waitKey(25);
+		if (c == 27)
+			break;
+		MVImg.release();
+	}
+	hexDrawLines.Release();
+	driveP->release();
+	findLines->release();
+	patLunLay.release();
+	HexLow.Release();
+	dummyImg.release();
+	delete driveP;
+	delete findLines;
+
+	destroyAllWindows();
+	return 0;
+}
+int testPi() {
+	int dummyMarker0;
+	int* pdummyMark0 = &dummyMarker0;
+	VideoCapture cap("IOFiles/linesV.avi");
+	if (!cap.isOpened()) {
+		cout << "Error\n";
+		return -1;
+	}
+	int frame_width = cap.get(CAP_PROP_FRAME_WIDTH);
+	int frame_height = cap.get(CAP_PROP_FRAME_HEIGHT);
+
+	Img MVImg;
+
+	Hex HexLow;
+	Img dummyImg;/*dummy image is just used to set dimensions for the first pass generation of the hexes*/
+	dummyImg.init(frame_width, frame_height, 3L);
+	HexLow.Init(&dummyImg);
+	PatLunaLayer patLunLay;
+	patLunLay.init(&HexLow);
+	LineFinder* findLines = new LineFinder;
+	findLines->init(&patLunLay.getPlateLayer(0));
+	DrivePlane* driveP = new DrivePlane;
+	driveP->init(findLines);
+
+	DrawHexImg hexDrawLines;
+	hexDrawLines.Init((HexBase*)&HexLow);
 	DrawHexImg hexDrawDriveP;
 	hexDrawDriveP.Init(driveP->getHexPlate(0), HexLow.getHexMaskPlus());
 	int dummyMarker1;
@@ -66,14 +134,14 @@ int testPi() {
 		unsigned char* frmdat = frame0.data;
 		MVImg.initNoOwn(frmdat, frame_width, frame_height, 3L);
 		HexLow.Update(&MVImg);
-		//colLay.Update();
 
 		patLunLay.Update();
 		findLines->spawn();
 		driveP->update();
-		hexDrawDriveP.Run();
+		hexDrawLines.renderLineImg(findLines);
+		//hexDrawDriveP.Run();
 
-		unsigned char* MVImgdat = hexDrawDriveP.getHexedImg()->getImg();//hexDrawLines.getHexedImg()->getImg();//doCol ? hexBaseDraw.getHexedImg()->getImg() : hexDraw.getHexedImg()->getImg();//MVImg.getImg();
+		unsigned char* MVImgdat = hexDrawLines.getHexedImg()->getImg();//hexDrawDriveP.getHexedImg()->getImg();//doCol ? hexBaseDraw.getHexedImg()->getImg() : hexDraw.getHexedImg()->getImg();//MVImg.getImg();
 		//unsigned char* MVImgdat = doCol ? hexBaseDraw.getHexedImg()->getImg() : hexDraw.getHexedImg()->getImg();//MVImg.getImg();
 		Size frameSize(frame_width, frame_height);
 		Mat rendFrame(frameSize, CV_8UC3, (void*)MVImgdat);
@@ -86,7 +154,8 @@ int testPi() {
 
 		cnt++;
 	}
-
+	hexDrawDriveP.Release();
+	hexDrawLines.Release();
 	driveP->release();
 	findLines->release();
 	patLunLay.release();
@@ -103,7 +172,7 @@ int runTest0() {
 	int dummyMarker0;
 	int* pdummyMark0 = &dummyMarker0;
 
-	VideoCapture cap("IOFiles/messylines.avi");
+	VideoCapture cap("IOFiles/linesV.avi");
 	if (!cap.isOpened()) {
 		cout << "Error\n";
 		return -1;
